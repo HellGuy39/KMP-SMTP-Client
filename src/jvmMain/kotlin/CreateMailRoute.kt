8ddf3011a158b4ed135733617c66b04159cf4fa5
HellGuy39 <alexey2005yt@gmail.com>
2023-12-11 12:31:02 +0300

    Add @Composable for CreateEmailRoute

--- src/jvmMain/kotlin/CreateMailRoute.kt
+++ src/jvmMain/kotlin/CreateMailRoute.kt
@@ -2,11 +2,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
 
+@Composable
 fun CreateEmailRoute() {
     val scaffoldState = rememberScaffoldState()
     val scope = rememberCoroutineScope()
